e09bf76926fd25a7aece0cf7d000cdb60f42e26b
Reynolds-OK <boakyeokyere96@gmail.com>
2023-06-04 12:54:03 +0000

    changes in about page

--- src/controllers/universal_controller.py
+++ src/controllers/universal_controller.py
@@ -148,7 +148,7 @@ def view_profile_all(userId, usertype):
 @web_app.route('/about', methods=['GET', 'POST'])
 def about():
     '''About page controller'''
-    msg=""
+    msg=" "
 
     # change tab value for logout/login
     tabs = {'log_status':'Log in / Sign up', 'add_home':''}
@@ -157,17 +157,23 @@ def about():
 
         tabs['add_home'] = 'Add Home' if session['usertype'] == 'owner' else ""
     
-    all_homes = Homes.get_homes()
+        # all_homes = Homes.get_homes()
 
-    if request.method == 'POST' and request.form['senderemail'] and request.form['sendername']\
-    and request.form['message']:
-        message =  f"Your message: \t{request.form['message']} \n\nThank you for your message, customer service will get back to you soon"
-        EmailNotifier.send_email(request.form['senderemail'], message, request.form['sendername'])
-        msg="Message was sent successfully"
+        if request.method == 'POST' and request.form['senderemail'] and request.form['sendername']\
+        and request.form['message']:
+            message =  f"Your message: \t{request.form['message']} \n\nThank you for your message, customer service will get back to you soon"
+            EmailNotifier.send_email(request.form['senderemail'], message, request.form['sendername'])
+            msg="Message was sent successfully"
+            
+            return render_template('about.html.j2', data=tabs, color='green', about_msg=msg)
+            
+        if request.method == 'POST':
+            msg="*Sorry, some required information are missing"
+            
+    elif request.method == 'POST':
+        msg = "Please Log in first"
         
-        return render_template('about.html.j2', data=tabs, homes=all_homes, about_msg=msg)
-
-    return render_template('about.html.j2', data=tabs, homes=all_homes, about_msg=msg)
+    return render_template('about.html.j2', data=tabs, color='red', about_msg=msg)
 
     
 @web_app.route('/updateprofile', methods =['GET','POST'])
